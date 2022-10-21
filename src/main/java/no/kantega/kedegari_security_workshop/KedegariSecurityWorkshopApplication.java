ca9468958d55d3827283b0f526a236ffea34c320
Frank Aakvik <frank.aakvik@kantega.no>
2022-10-21 11:04:18 +0200

    Initial commit

---
+++ src/main/java/no/kantega/kedegari_security_workshop/KedegariSecurityWorkshopApplication.java
@@ -0,0 +1,13 @@
+package no.kantega.kedegari_security_workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KedegariSecurityWorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KedegariSecurityWorkshopApplication.class, args);
+    }
+
+}
